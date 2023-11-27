12383ff00cf0a60eec6765b7b9d99090c6cab45a
aheath <aaron.heath5447@gmail.com>
2023-11-26 21:55:04 -0500

    Change salt back to byte array

--- src/main/java/com/aheath/db/UserDAO.java
+++ src/main/java/com/aheath/db/UserDAO.java
@@ -18,5 +18,30 @@ public interface UserDAO {
             "(:firstname, :lastname, :email, :pw_hash, :phone, :zip, :salt)" +
             "RETURNING user_id")
     @GetGeneratedKeys
-    int createUser(@Bind("firstname") String firstname,@Bind("lastname") String lastname, @Bind("email") String email, @Bind("pw_hash") String pw_hash, @Bind("phone") String phone, @Bind("zip") String zip, @Bind("salt") String salt);
+    int createUser(@Bind("firstname") String firstname,@Bind("lastname") String lastname, @Bind("email") String email, @Bind("pw_hash") String pw_hash, @Bind("phone") String phone, @Bind("zip") String zip, @Bind("salt") byte[] salt);
+
+    // Method for authentication
+
+    /**
+     *
+     * @param email entered by user and passed by front end application
+     * @return salt for user if the user exists
+     */
+    @SqlQuery("SELECT salt FROM user_account WHERE email = :email")
+    String getSalt(@Bind("email") String email);
+
+
+    @SqlQuery("SELECT user_id, firstname, lastname, email, phone, zip FROM user_account WHERE email= :email pw_hash = :pw_hash AND salt = :salt")
+    User checkCredentials(@Bind("email") String email, @Bind("pw_hash") String pw_hash, @Bind("salt") String salt);
+//
+//    public default User authenticate(String email, String pw_hash) {
+//        User user = null;
+//
+//        // Get and store salt from db
+//        String salt = this.getSalt(email);
+//
+//
+//
+//        return user;
+//    }
 }
