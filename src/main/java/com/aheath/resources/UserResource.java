35d324a887776eca889ab7a14cd6242bda85e83d
aheath <aaron.heath5447@gmail.com>
2023-11-26 21:55:44 -0500

    Move salt generation to encoder class. Password hashing and validation next

--- src/main/java/com/aheath/resources/UserResource.java
+++ src/main/java/com/aheath/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.aheath.resources;
 
 import com.aheath.api.User;
 import com.aheath.db.UserDAO;
+import com.aheath.security.PasswordEncoder;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.jdbi.v3.core.Jdbi;
@@ -9,10 +10,8 @@ import org.jdbi.v3.core.Jdbi;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
-import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 
@@ -20,6 +19,7 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
     private final UserDAO userDAO;
+    private PasswordEncoder passwordEncoder;
 
     public UserResource(Jdbi jdbi) {
         this.userDAO = jdbi.onDemand(UserDAO.class);
@@ -36,6 +36,10 @@ public class UserResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public User authenticateUser(User user) {
         // TODO: Implement authentication
+        // USER SHOULD ONLY HAVE EMAIL and PW
+
+        // get salt from user if exists.
+
         return user;
     }
 
@@ -43,22 +47,24 @@ public class UserResource {
     @Path("/register")
     @Consumes(MediaType.APPLICATION_JSON)
     public int createUser(User user) {
-        // TODO: Re-write create user creation using Base64
         //https://stackoverflow.com/questions/2860943/how-can-i-hash-a-password-in-java
-        //TODO: Review Generate Random Salt
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
-        random.nextBytes(salt);
-        //TODO: Convert salt to string
+//        SecureRandom random = new SecureRandom();
+//        byte[] salt = new byte[16];
+//        random.nextBytes(salt);
         Base64.Encoder encoder = Base64.getEncoder();
 //        System.out.printf("salt: %s%n", encoder.encodeToString(salt));
+        //   Instantiate password encoder.
+        try {
+            passwordEncoder = new PasswordEncoder();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+//        Store salt string in user
+        user.setSalt(this.passwordEncoder.generateSalt());
 
-        //TODO: set salt
-        user.setSalt(encoder.encodeToString(salt));
-
-        KeySpec spec = new PBEKeySpec(user.getPw_hash().toCharArray(),salt,65536,128);
+//        Encrypt Password
+        KeySpec spec = new PBEKeySpec(user.getPw_hash().toCharArray(),user.getSalt(),65536,128);
         SecretKeyFactory factory = null;
-        // This works, but it currently stores the HASH with the brackets and quotes. It's uggo. I'd rather concatenate them
         try {
             factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
         } catch (NoSuchAlgorithmException e) {
--- src/main/java/com/aheath/security/PasswordEncoder.java
+++ src/main/java/com/aheath/security/PasswordEncoder.java
@@ -1,2 +1,43 @@
-package com.aheath.security;public class PasswordEncoder {
+package com.aheath.security;
+
+import javax.crypto.SecretKeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class PasswordEncoder {
+    //    Encodes password + salt hash as a string
+    private Base64.Encoder encoder = Base64.getEncoder();
+
+    //    Creates Hash of password + salt
+    private SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+    //    This is for generating salts
+    private SecureRandom random = new SecureRandom();
+
+    private final int iterationCount = 65536;
+    private final int keyLength = 128;
+
+    public PasswordEncoder() throws NoSuchAlgorithmException {
+    }
+
+//    //    Security Methods
+//    public boolean validate(String inputPassword,String validPassword, String salt) {
+//        //  Hash Input Password using Salt
+//        KeySpec spec = new PBEKeySpec(inputPassword.toCharArray(), salt, this.iterationCount, this.keyLength);
+//
+//        //
+//    }
+
+    /**
+     * This method takes no parameters. It generates a string to be used as a salt for password encryption and storage.
+     *
+     * @return a String salt to be used when encrypting passwords for storage.
+     */
+    public byte[] generateSalt() {
+
+        byte[] salt = new byte[16];
+        this.random.nextBytes(salt);
+        return salt;
+    }
 }
