ab00f822fa1e0c9e92f8d4905102ca0270cf8402
aheath <aaron.heath5447@gmail.com>
2024-02-05 21:24:25 -0500

    Implement user registration. Routes running and functioning

--- src/main/java/com/plantpoppa/auth/resources/UserResource.java
+++ src/main/java/com/plantpoppa/auth/resources/UserResource.java
@@ -1,16 +1,21 @@
 package com.plantpoppa.auth.resources;
 
 import com.plantpoppa.auth.dao.UserRepository;
+import com.plantpoppa.auth.models.Session;
 import com.plantpoppa.auth.models.User;
 import com.plantpoppa.auth.models.UserDto;
 import com.plantpoppa.auth.services.UserService;
 import jakarta.inject.Inject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
@@ -26,6 +31,22 @@ public class UserResource {
     @GetMapping("/")
     List<UserDto> findAllUsers() {
         return userService.findAllUsers();
+    }
+
+    @PostMapping(value="/register",
+    consumes=MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<?> createUser(@RequestBody UserDto userDto) {
+        Optional<UserDto> createdUser = userService.createUser(userDto);
+
+        if(createdUser.isEmpty()) {
+            Map<String, String> response = null;
+            response.put("message", "Something went wrong");
+
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(createdUser,
+                HttpStatus.OK);
     }
 
     @PutMapping(value="/firstname",
