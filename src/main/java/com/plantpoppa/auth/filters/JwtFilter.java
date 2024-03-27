baf1e2b5d29f3c1b927c9b023bd18937052858cf
aheath <aaron.heath5447@gmail.com>
2024-03-26 22:21:38 -0400

    Begin token filtering. Currently returns 401 if no AUTHORIZATION header is provided

--- src/main/java/com/plantpoppa/auth/filters/JwtFilter.java
+++ src/main/java/com/plantpoppa/auth/filters/JwtFilter.java
@@ -1,15 +1,33 @@
 package com.plantpoppa.auth.filters;
 
+import com.plantpoppa.auth.services.JwtService;
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Component
 @Order(2)
 public class JwtFilter implements Filter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
 
     @Override
     public void doFilter(ServletRequest request,
@@ -22,11 +40,28 @@ public class JwtFilter implements Filter {
             return;
         }
 
-
-
+        Optional<String> tokenParam = Optional.ofNullable(req.getHeader("AUTHORIZATION"));
+        if(tokenParam.isEmpty()) {
+            sendUnauthorizedResponse(response);
+        }
+        //TODO: Continue token validation
 
         // continue
         chain.doFilter(request, response);
+    }
 
+    private void sendUnauthorizedResponse(ServletResponse response) {
+        HttpServletResponse res = (HttpServletResponse) response;
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("message", "No Authorization Header");
+
+        res.setStatus(HttpStatus.UNAUTHORIZED.value());
+        res.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        try {
+            res.sendError(401,"No Authorization Header");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
