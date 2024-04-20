0b75cc3be603736d9c0a57319625f334cd422ac3
aheath <aaron.heath5447@gmail.com>
2024-04-20 09:30:20 -0400

    Remove imports from user.java

--- src/main/java/com/plantpoppa/auth/models/User.java
+++ src/main/java/com/plantpoppa/auth/models/User.java
@@ -1,9 +1,7 @@
 package com.plantpoppa.auth.models;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 import java.util.UUID;
 
