bb8aed62a1c73a62d8ab3f9bdeeb6bf9b875d7d8
aheath <aaron.heath5447@gmail.com>
2024-04-20 12:21:06 -0400

    Accept CORS from plantpoppa domain

---
+++ src/main/java/com/plantpoppa/auth/security/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.plantpoppa.auth.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://plantpoppa.com"); // enable CORS for plantpoppa.com
+            }
+        };
+    }
+}
